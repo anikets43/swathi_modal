2b82cb6407af61f0f85deb1f32cf66ad4a102064
Aniket <anikets43@gmail.com>
2018-06-23 17:04:28 +0530

    Commiting all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from "@angular/core";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { BsModalRef } from "ngx-bootstrap/modal/bs-modal-ref.service";
+import { setTheme } from "ngx-bootstrap/utils";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "Modal Example";
+  modalRef: BsModalRef;
+  name = "Default Value";
+  constructor(private modalService: BsModalService, private http: HttpClient) {
+    setTheme("bs3"); // or 'bs4'
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  onClose() {
+    this.modalRef.hide();
+    this.modalRef = null;
+    // Call your API Here
+
+    this.http.get("assets/config.json").subscribe(
+      data => {
+        alert("API succcess");
+      },
+      err => {
+        alert("API Failure");
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BsModalService, ModalModule } from "ngx-bootstrap/modal";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, ModalModule.forRoot()],
+  bootstrap: [AppComponent],
+  providers: [BsModalService]
 })
-export class AppModule { }
+export class AppModule {}
